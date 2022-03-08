b86308d0a2821cf1b188b80fc2a5469a99098ad7
Mason Lee <93163309+mh0805@users.noreply.github.com>
2022-03-07 22:50:15 -0800

    Update Olivander's_Inventory.sql

--- Basic Join/Olivander's_Inventory.sql
+++ Basic Join/Olivander's_Inventory.sql
@@ -10,3 +10,18 @@ WHERE wp.is_evil = 0 AND
                         JOIN wands_property AS wp2 ON w2.code = wp2.code
                         WHERE wp.age = wp2.age AND w.power = w2.power)
 ORDER BY w.power DESC, wp.age DESC
+
+/*This one is a query that works with MS SQL Server.
+I tried a subquery without using MIN() function and instead partitioned data by the wand code and power
+and ordered them in power DESC and coins_needed ASC, so this way our ideal high-power/low-cost wand wil be chosen, and have this as
+row number. Grabbing the first row would provide us the ideal combination of high power/low cost/older wand.*/
+SELECT id, age, coins_needed, power
+FROM
+(SELECT w.id, wp.age, w.coins_needed, w.power,
+    ROW_NUMBER() OVER (PARTITION BY w.code, w.power ORDER BY w.power DESC, w.coins_needed ASC) AS row_number
+FROM Wands AS w
+    JOIN Wands_Property AS wp ON w.code = wp.code
+WHERE wp.is_evil = 0) AS sub_query
+
+WHERE sub_query.row_number = 1
+ORDER BY power DESC, age DESC
